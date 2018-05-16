684e2729309efde94ddb9bbbeaad62f3cce686dd
cyberguy9000 <39337895+cyberguy9000@users.noreply.github.com>
2018-05-16 11:41:32 -0500

    testing loops
    iterate through word print each letter, iterate through an array print each result.

---
+++ starships.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# Testing out loops
+#
+
+for let in 'Starship':
+        print 'Current Letter:', let
+
+
+starships = ['Akira', 'Centaur', 'Intrepid','Galen']
+for starship in starships:
+        print 'Current starship :', starship
+
+print "Good job!"
